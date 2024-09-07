7c7392bca2606fd806ddeb6f4ac1aac2226a89af
Kayla Flint <bookwm2016@gmail.com>
2024-09-07 16:24:49 -0400

    made new repo and copied my code that I made in my scratch folder into it

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# tes1
+
+## Description:
+makes a README FILE
+
+## Installation:
+```
+123
+```
+
+## Usage:
+to make a professional README file
+
+## Contributors:
+me
+
+## License:
+This project is licensed under the MIT license.
+
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: [BookWM101](https://github.com/BookWM101)
+- Email: blah
---
+++ index.js
@@ -0,0 +1,83 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Questions to gather info for README
+const questions = [
+  {
+    type: 'input',
+    name: 'projectTitle',
+    message: "What's the title of your project?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation steps?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use the project? / Usage?',
+  },
+  {
+    type: 'input',
+    name: 'contributors',
+    message: 'Who contributed to your project?'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What license did you use?',
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: "What's your GitHub username?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What's your email address?",
+  },
+];
+
+// Generate README content
+const generateREADME = (answers) => `
+# ${answers.projectTitle}
+
+## Description:
+${answers.description}
+
+## Installation:
+\`\`\`
+${answers.installation}
+\`\`\`
+
+## Usage:
+${answers.usage}
+
+## Contributors:
+${answers.contributors}
+
+## License:
+This project is licensed under the ${answers.license} license.
+
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})
+- Email: ${answers.email}
+`;
+
+// To Run App
+inquirer.prompt(questions).then((answers) => {
+  const readmeContent = generateREADME(answers);
+
+  // To Write it into a README file
+  fs.writeFileSync('README.md', readmeContent);
+  console.log('README.md has been created successfully!');
+});
